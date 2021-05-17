6d8125c91cceb907eb308812dc3b6b05652b5a94
berbaroovez <carterolson3@gmail.com>
2021-05-17 15:14:31 -0500

    SEO FIXES

---
+++ next-seo.config.ts
@@ -0,0 +1,30 @@
+const title = "Any Tubers";
+const description = "A tournament series for the community of Stanz";
+
+const SEO = {
+  title,
+  description,
+  canonical: "https://www.anytubbers.com",
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://www.anytubbers.com",
+    title,
+    description,
+    images: [
+      {
+        url: "https://www.anytubbers.com/og.png",
+        alt: title,
+        width: 1280,
+        height: 720,
+      },
+    ],
+  },
+  twitter: {
+    handle: "@berbaroovez",
+    site: "@berbaroovez",
+    cardType: "summary_large_image",
+  },
+};
+
+export default SEO;
--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,14 @@
 import GlobalStyles from "../util/GlobalStyles";
 import { DefaultSeo } from "next-seo";
-
+import SEO from "../next-seo.config";
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyles />
-      <DefaultSeo
-        title="Any Tubbers"
-        defaultTitle="Any Tubbers"
-        openGraph={{
-          type: "website",
-          locale: "en_IE",
-          url: "https://anytubbers.com/",
-          site_name: "Any Tubbers",
-          images: [
-            {
-              url: "https://www.anytubbers.com/og.png",
-              alt: "Any Tubbers",
-              width: 1280,
-              height: 720,
-            },
-          ],
-        }}
-        twitter={{
-          handle: "@Berbaroovez",
-          site: "@Berbaroovez",
-          cardType: "summary_large_image",
-        }}
-      />
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <DefaultSeo {...SEO} />
 
       <Component {...pageProps} />
     </>
